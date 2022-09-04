07cec24a5f1f60734504e557556a0028d0094010
mindscan-de <maxim.gansert@gmail.com>
2022-09-04 19:51:23 +0200

    fill basic implementation for DocumentLabelAndMetadata

--- LatentIron-Indexer/src/de/mindscan/latentiron/document/DocumentLabelAndMetadata.java
+++ LatentIron-Indexer/src/de/mindscan/latentiron/document/DocumentLabelAndMetadata.java
@@ -25,9 +25,111 @@
  */
 package de.mindscan.latentiron.document;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 /**
  * 
  */
 public class DocumentLabelAndMetadata {
+
+    private String documentKey;
+    private String documentLocation;
+    private String documentSimpleFilename;
+
+    private long fileSize = 0L;
+
+    private Map<String, String> labelAndMetadataMap;
+
+    /**
+     * 
+     */
+    public DocumentLabelAndMetadata( String documentKey, String documentLocation, String documentSimpleFilename ) {
+        this.documentKey = documentKey;
+        this.documentLocation = documentLocation;
+        this.documentSimpleFilename = documentSimpleFilename;
+        this.labelAndMetadataMap = new HashMap<>();
+    }
+
+    public String getDocumentKey() {
+        return documentKey;
+    }
+
+    public String getDocumentLocation() {
+        return documentLocation;
+    }
+
+    public String getDocumentSimpleFilename() {
+        return documentSimpleFilename;
+    }
+
+    public void addLabel( String labelKey, String labelValue ) {
+        this.labelAndMetadataMap.put( labelKey, labelValue );
+    }
+
+    public void containsLabel( String labelKey ) {
+        this.labelAndMetadataMap.containsKey( labelKey );
+    }
+
+    public Map<String, String> getLabelAndMetadataMap() {
+        return labelAndMetadataMap;
+    }
+
+    /** 
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        return documentKey + "@" + documentLocation;
+    }
+
+    public long getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize( long fileSize ) {
+        this.fileSize = fileSize;
+    }
+
+    public Collection<String> getAllValuesLowercase() {
+        Set<String> result = new HashSet<>();
+
+        result.add( getDocumentSimpleFilename().toLowerCase() );
+        result.add( getDocumentLocation().toLowerCase() );
+        result.add( Long.toString( getFileSize() ) );
+
+        List<String> collectedLowercaseValues = labelAndMetadataMap.values().stream().map( s -> s.toLowerCase() ).collect( Collectors.toList() );
+        result.addAll( collectedLowercaseValues );
+
+        return result;
+    }
+
+    public Collection<String> getAllValuesOriginalcase() {
+        Set<String> result = new HashSet<>();
+
+        result.add( getDocumentSimpleFilename() );
+        result.add( getDocumentLocation() );
+        result.add( Long.toString( getFileSize() ) );
+        result.addAll( labelAndMetadataMap.values() );
+
+        return result;
+    }
+
+    public Map<String, String> getKeyValuesAsMap() {
+        Map<String, String> result = new HashMap<>();
+
+        result.put( "file.simplename", getDocumentSimpleFilename() );
+        result.put( "file.path", getDocumentLocation() );
+        result.put( "file.size", Long.toString( getFileSize() ) );
+
+        result.putAll( labelAndMetadataMap );
+
+        return result;
+    }
 
 }
