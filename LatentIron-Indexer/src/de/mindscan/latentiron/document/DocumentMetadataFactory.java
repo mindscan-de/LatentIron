7e5588bf12b777d63f49f2977fe84124e1fdc7d4
mindscan-de <maxim.gansert@gmail.com>
2022-09-04 20:58:32 +0200

    rename class; split in static file metadata and content data

--- LatentIron-Indexer/src/de/mindscan/latentiron/document/DocumentMetadataFactory.java
+++ LatentIron-Indexer/src/de/mindscan/latentiron/document/DocumentMetadataFactory.java
@@ -32,7 +32,7 @@ import java.nio.file.Path;
 /**
  * 
  */
-public class DocumentLabelAndMetadataFactory {
+public class DocumentMetadataFactory {
 
     public static DocumentMetadata createDocumentLabelAndMetadata( DocumentId documentId, Path fileToIndex ) {
 
